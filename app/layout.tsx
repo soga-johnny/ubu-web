d8f0fd1c3a135d57a590cad6836449a51adf2ce6
soga-johnny <soga.johnny@gmail.com>
2025-02-26 14:58:47 +0900

    add ogp

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,28 @@ const zenKaku = Zen_Kaku_Gothic_Antique({
 export const metadata: Metadata = {
   title: "ubu book store",
   description: "日本からカルチャーを産む、大阪・中津の本屋さん",
+  openGraph: {
+    type: 'website',
+    locale: 'ja_JP',
+    url: 'https://ubu-web.vercel.app/',
+    title: 'ubu book store',
+    description: '日本からカルチャーを産む、大阪・中津の本屋さん',
+    siteName: 'UBU書店',
+    images: [
+      {
+        url: '/ogp.png',
+        width: 1280,
+        height: 720,
+        alt: 'ubu book store',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'ubu book store',
+    description: '日本からカルチャーを産む、大阪・中津の本屋さん',
+    images: ['/ogp.png'],
+  },
 };
 
 export default function RootLayout({
